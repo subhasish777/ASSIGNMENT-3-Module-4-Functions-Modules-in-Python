9f473a8c2b25fd85982a54ac4e797ed5f432921f
subhasish777 <57346327+subhasish777@users.noreply.github.com>
2025-09-26 20:05:25 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,12 @@
+
+n=int(input("enter a number"))
+def factorial(n):
+
+    if n < 2:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+result = factorial(n)
+
+print('factorial of',n, 'is' , result)
---
+++ mathmodule.py
@@ -0,0 +1,8 @@
+import math
+
+n = float(input("enter a number"))
+
+
+print("The square root of the number is", math.sqrt(n))
+print("The log of the number is", math.log(n))
+print("The sine of the number is", math.sin(n))
